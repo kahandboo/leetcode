dc338aa92d97f5c17f147383f380325c771f92b6
kahandboo <158439807+kahandboo@users.noreply.github.com>
2024-09-05 10:24:54 +0900

    Time: 11 ms (84.82%), Space: 9.2 MB (83.82%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,18 +1,7 @@
 int singleNumber(int* nums, int numsSize) {
-    int count = 0; //
-    if (numsSize == 1) return *nums;
-    else{
-        for(int i=0; i<numsSize; i++){
-            int temp = *(nums+i);
-            for(int j=0; j<numsSize; j++){
-                if((temp != *(nums + j))){ 
-                    count++;
-                }
-            }
-            if(count == numsSize - 1){return temp;}
-            count = 0;
-        }
+    int result = 0;
+    for(int i=0; i<numsSize; i++){
+        result ^= *(nums + i);
     }
-    return 0;
-
+    return result;
 }
