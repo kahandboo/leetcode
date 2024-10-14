dc5d8576fc77eee00b4970adf959bea53e192263
kahandboo <158439807+kahandboo@users.noreply.github.com>
2024-10-14 21:02:34 +0900

    Time: 99 ms (12.86%), Space: 21.2 MB (70.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -12,7 +12,9 @@ class Solution:
                 my_dict[i] = 1
 
         for i in my_dict:
-            heapq.heappush(queue, (-my_dict[i],i))
+            heapq.heappush(queue, (my_dict[i],i))
+            if len(queue) > k:
+                heapq.heappop(queue)
 
         for _ in range(k):
             result.append(heapq.heappop(queue)[1])
