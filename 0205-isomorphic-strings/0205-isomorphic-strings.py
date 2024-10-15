e28786b682d14e9bb03f1f699fbfd348590d8dfd
kahandboo <158439807+kahandboo@users.noreply.github.com>
2024-10-15 20:19:40 +0900

    Time: 48 ms (24.89%), Space: 16.7 MB (49.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,15 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        i = 0
-        hashtable = {}
-        while i < len(s):
-            if s[i] in hashtable.keys():
-                if hashtable[s[i]] != t[i]:
-                    return False
-            elif t[i] in hashtable.values():
-                if s[i] != [k for k, v in hashtable.items() if v == t[i]]:
+
+        s_to_t = {}
+        t_to_s = {}
+
+        for char_s, char_t in zip(s, t):
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
                     return False
             else:
-                hashtable[s[i]] = t[i]
-            i += 1
+                s_to_t[char_s] = char_t
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+
         return True
